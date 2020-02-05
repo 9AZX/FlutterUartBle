13ce95c2e931bcca876434815910858fb94f0f60
Jon Ihlas <jon.i@hotmail.fr>
2020-02-05 12:09:16 +0100

    Add flutter_ble_lib to dependencies

--- lib/flutter_uart_ble.dart
+++ lib/flutter_uart_ble.dart
@@ -1,7 +1,3 @@
 library flutter_uart_ble;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+import 'package:flutter_ble_lib/flutter_ble_lib.dart';
